84078d82ed98261a9d47ccb85744e84e595fc53b
Benoît Zugmeyer <bzugmeyer@gmail.com>
2012-12-10 23:26:11 +0100

    Version bump

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def readme():
         return f.read()
 
 setup(name='addic7ed',
-      version='0.1.4',
+      version='0.1.5',
       description='A commandline access to addic7ed subtitles',
       long_description=readme(),
       url='https://github.com/BenoitZugmeyer/addic7ed-cli/',
