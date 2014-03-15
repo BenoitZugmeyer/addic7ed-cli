c47aca3c2c865ca821445fa92f2bdd632f527a53
Benoît Zugmeyer <bzugmeyer@gmail.com>
2014-03-15 18:32:03 +0100

    Configuration file support

--- addic7ed/__init__.py
+++ addic7ed/__init__.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python2
 
+import ConfigParser
 import os.path
 from urlparse import urljoin
 from pyquery import PyQuery as query
@@ -273,7 +274,7 @@ class UI(object):
                 query = args.query
 
             if args.release:
-                release = string_set(' '.join(args.release))
+                release = string_set(args.release)
 
             if args.verbose:
                 print 'Using query "{query}" and release "{release}"'.format(
@@ -394,16 +395,116 @@ def string_set(string):
     return set(string.split(' ')) if string else set()
 
 
+class Arguments(object):
+    def __init__(self):
+        self.language = []
+        self.default_language = []
+        self.verbose = False
+        self.hearing_impaired = False
+        self.batch = False
+        self.brute_batch = False
+        self.query = None
+        self.release = None
+        self.overwrite = False
+        self.ignore = False
+
+    @staticmethod
+    def get_paths():
+        # Linux and OSX:
+        paths = [
+            os.path.join(os.path.expanduser('~'), '.config', 'addic7ed'),
+            os.path.join(os.path.expanduser('~'), '.addic7ed'),
+        ]
+
+        # Windows
+        if 'APPDATA' in os.environ:
+            paths.append(os.path.join(os.environ['APPDATA'], 'config'))
+
+        return paths
+
+    def read_defaults(self):
+        paths = self.get_paths()
+        valid_paths = (path for path in paths if os.path.isfile(path))
+        configuration_path = next(valid_paths, None)
+
+        self.default_language = []
+
+        if configuration_path is not None:
+            config = ConfigParser.ConfigParser(allow_no_value=True)
+            config.read(configuration_path)
+
+            if config.has_section('flags'):
+                self._read_flags(config)
+
+            if config.has_section('languages'):
+                self._read_languages(config)
+
+    def _read_flags(self, config):
+        def getbool(name, default=False):
+            if not config.has_option('flags', name):
+                return default
+
+            value = config.get('flags', name)
+            if isinstance(value, basestring):
+                return config.getboolean('flags', name)
+
+            return value is None
+
+        self.verbose = getbool('verbose')
+        self.hearing_impaired = getbool('hearing-impaired')
+        self.batch = getbool('batch')
+        self.brute_batch = getbool('brute-batch')
+        self.overwrite = getbool('overwrite')
+        self.ignore = getbool('ignore')
+
+    def _read_languages(self, config):
+        self.default_language = [l for l, v in config.items('languages')
+                                 if v is not False]
+
+    def finalize(self):
+        if not self.language:
+            self.language = self.default_language[:]
+
+
 def main():
 
     import argparse
+    import textwrap
+
+    epilog = '''
+    Configuration file:
+
+      You can store frequently used options in a configuration file. Create a
+      file at ~/.config/addic7ed (Linux, OSX) or %APPDATA%/config (Windows),
+      and it will be parsed using the Python ConfigParser (see example below).
+      It can contain two sections:
+
+      * [flags], to set a flag (verbose, hearing-impaired, overwrite, ignore,
+        batch or brute-batch)
+
+      * [languages], to list prefered languages
+
+      Example:
+
+        [flags]
+        hearing-impaired = no
+        batch
+
+        [languages]
+        french
+        english
+
+    Video organizer:
+      video-organizer format is supported. If a "filelist" file is next to an
+      episode, it will use it to extract its real name and forge the good
+      query. See https://github.com/JoelSjogren/video-organizer for further
+      informations.
+    '''
+
     parser = argparse.ArgumentParser(
         description='Downloads SRT files from addic7ed.com.',
-        epilog='Note: video-organizer format is supported. If a "filelist" '
-        'file is next to an episode, it will use it to extract its real name '
-        'and forge the good query. See \n'
-        'https://github.com/JoelSjogren/video-organizer for further '
-        'informations.')
+        epilog=textwrap.dedent(epilog),
+        formatter_class=argparse.RawDescriptionHelpFormatter)
 
     parser.add_argument('file', nargs='+',
                         help='Video file name.')
@@ -415,7 +516,7 @@ def main():
     custom.add_argument('-q', '--query',
                         help='Custom query. (default: based on the filename)')
 
-    custom.add_argument('-r', '--release', action='append', default=[],
+    custom.add_argument('-r', '--release',
                         help='Custom release. (default: based on the '
                         'filename)')
 
@@ -425,6 +526,9 @@ def main():
 
     custom.add_argument('-H', '--hearing-impaired', action='store_true',
                         help='Prefer hearing impaired version.')
+
+    custom.add_argument('--no-hearing-impaired', dest='hearing_impaired',
+                        action='store_false')
 
     batch = parser.add_argument_group('Automation')
     batch.add_argument('-o', '--overwrite', action='store_true',
@@ -442,7 +546,12 @@ def main():
                        help='Do not ask anything, get the best matching '
                        'subtitle. Use the first result of the search.')
 
-    args = parser.parse_args()
+    args = Arguments()
+    args.read_defaults()
+
+    parser.parse_args(namespace=args)
+
+    args.finalize()
 
     try:
         for file in args.file:
