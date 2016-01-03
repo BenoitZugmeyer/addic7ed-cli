97abac8a8dcc0faeba5c85d7a4b738255f636c36
Benoît Zugmeyer <bzugmeyer@gmail.com>
2016-01-03 15:00:42 +0100

    Memoize get_current_user result

--- addic7ed_cli/login.py
+++ addic7ed_cli/login.py
@@ -4,26 +4,40 @@ from addic7ed_cli.error import Error
 
 
 def login(user, password):
+    global _current_user
+
     result = session.post('/dologin.php', data={
         'username': user,
         'password': password,
         'remember': 'true'
     })
 
-    if get_current_user(result):
+    _current_user = None
+
+    if get_current_user():
         return session.cookies['PHPSESSID']
 
     raise Error(result('center p[align="center"]').text())
 
 
-def get_current_user(page=None):
-    if not page:
-        page = session.get('/')
+_current_user = None
 
-    titles = page('.footer_dropup h4')
-    if titles and titles[0].text.startswith('Welcome back'):
-        return titles[0].text[13:].strip()
+
+def get_current_user():
+    global _current_user
+
+    if _current_user is None:
+        page = session.get('/')
+        titles = page('.footer_dropup h4')
+        if titles and titles[0].text.startswith('Welcome back'):
+            _current_user = titles[0].text[13:].strip()
+        else:
+            _current_user = False
+
+    return _current_user
 
 
 def set_session(sess):
+    global _current_user
+    _current_user = None
     session.cookies['PHPSESSID'] = sess
